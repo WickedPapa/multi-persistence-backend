cb2939809eb882d849284000636b9d5e4852aac6
skymontanop <pamontan@emeal.nttdata.com>
2026-07-27 17:21:30 +0200

    feat: OpenApi contract test

---
+++ src/test/java/it/montano/multipersistencebackend/MongoOpenApiContractTest.java
@@ -0,0 +1,21 @@
+package it.montano.multipersistencebackend;
+
+import it.montano.multipersistencebackend.common.constant.Datasources;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@SpringBootTest(properties = {AbstractMongoIntegrationTest.PROPERTY_KEY_EQUALS + Datasources.MONGO})
+class MongoOpenApiContractTest extends AbstractMongoIntegrationTest {
+
+  @Autowired
+  @Qualifier("requestMappingHandlerMapping")
+  RequestMappingHandlerMapping handlerMapping;
+
+  @Test
+  void shouldExposeAllOperationsDeclaredInOpenApiSpec() {
+    OpenApiContractSupport.assertOpenApiOperationsAreExposed(handlerMapping);
+  }
+}
